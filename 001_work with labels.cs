caf3b42658695709ec026268c95bde5fef041d28
Md Sohel Rana <67967149+tirelesssohel@users.noreply.github.com>
2023-12-04 02:34:20 +0600

    Create 001_work with labels.cs

---
+++ 001_work with labels.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWithLabel_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello Programmers";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "My First Windows App";
+            label2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "This text is visible";
+        }
+    }
+}
